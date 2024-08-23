c41afc529a293fdeab0ef5fd80dcfbc98a979a2c
carvalhofg <carvalho_biel@outlook.com>
2024-08-23 00:52:42 -0300

    create vendor alias, change user path, update db connection in sqlalchemy, fix passing first element and save jit in database only after filtering

--- report_feeding/report/main/jit_generation.py
+++ report_feeding/report/main/jit_generation.py
@@ -1,16 +1,16 @@
 import os
 from datetime import datetime
-from report_feeding.data_source.file_source import FileDataSource as FileDS
+from report_feeding.data_source.database_source import DatabaseDataSource as DbSource
 from report_feeding.report.jit.layout_builder import LayoutBuilder
 from report_feeding.report.jit.data_filler import FillerData
 from data_loading.repositories.jit_repository import JitRepository
 from data_loading.services.jit_service import JitService
 from data_loading.config.database import session
+from openpyxl import load_workbook
 
 
 class JitGeneration:
     def __init__(self):
-        self.user_path = os.path.expanduser('~')
 
         self.current_datetime = datetime.now().strftime("%Y-%m-%d")
         
@@ -20,29 +20,29 @@ class JitGeneration:
         self.db_host = os.environ.get('DB_HOST', None)
         self.db_port = os.environ.get('DB_PORT', None)
         self.table_name = 'jit'
+        self.user_path = os.environ.get('STORE_PATH', None)
 
-        self.__SOURCE_PATH = f'{self.user_path}\\Documents\\otica-nany\\os\\os-{self.current_datetime}.csv'
-        self.__TARGET_FOLDER = f'{self.user_path}\\Documents\\otica-nany\\jit'
+        self.__TARGET_FOLDER = f'{self.user_path}\\otica-nany\\jit'
         self.__TARGET_PATH = f'{self.__TARGET_FOLDER}\\jit-{self.current_datetime}.xlsx'
         
         self.__REPRO_STRING = '19944 - REPRO PRODUTOS OPTICOS LTDA'
         
-        self.file_data_source = FileDS('csv', self.__SOURCE_PATH)
-        self.data = self.file_data_source.read_file()
-        
         self.jit_repository = JitRepository(session)
         self.jit_service = JitService(self.jit_repository)
+        
+        self.db_data_source = DbSource(self.jit_repository)
+        self.data = self.db_data_source.read_jit()
     
-    def __get_only_non_existant_data(self):
-        return [row for row in self.data if self.jit_service.check_existance_of_jit_by_os_number(row[1].replace('.', '').replace(',00', ''))]
+    def __get_only_existant_data(self):
+        return [jit for jit in self.data if self.jit_service.check_existance_of_jit_by_os_number(jit[1])]
     
     def __split_data_by_repro_and_store(self):
-        existant_data = self.__get_only_non_existant_data()
+        existant_data = self.__get_only_existant_data()
         non_existent_data = [row for row in self.data if row not in existant_data]
         
         repro_data = []
         store_data = []
-        for row in non_existent_data[1:]:
+        for row in non_existent_data:
             if self.__REPRO_STRING in row:
                 repro_data.append(row)
             else:
@@ -50,18 +50,31 @@ class JitGeneration:
                 
         return repro_data, store_data
     
+    def __is_xlsx_empty(self, file_path):
+        if self.__is_file_created(file_path):
+            workbook = load_workbook(file_path, read_only=True)
+            first_sheet = workbook.worksheets[0]
+            first_cell = first_sheet['A1']
+            return first_cell.value is None or first_cell.value == ""
+        
+        return True
+    
+    def __is_file_created(self, file_path):
+        return os.path.exists(file_path)
+    
     def generate(self):
         repro_data, store_data = self.__split_data_by_repro_and_store()
         repro_data_size = len(repro_data)
         full_data = store_data + repro_data
         
         os.makedirs(self.__TARGET_FOLDER, exist_ok=True)
-        
-        layout_builder = LayoutBuilder(len(full_data), self.__TARGET_PATH, repro_data_size)
-        layout_builder.build()
 
-        filler_data = FillerData(self.__TARGET_PATH, full_data)
-        filler_data.fill()
+        if self.__is_xlsx_empty(self.__TARGET_PATH):
+            layout_builder = LayoutBuilder(len(full_data), self.__TARGET_PATH, repro_data_size)
+            layout_builder.build()
+
+            filler_data = FillerData(self.__TARGET_PATH, full_data)
+            filler_data.fill()
 
 if __name__ == "__main__":
     jit_generation = JitGeneration()
